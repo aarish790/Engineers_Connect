97755a1e1e69ec80a5b45395f1bce073cf3c2db0
unknown <mdaarishsid790@gmail.com>
2020-07-10 15:22:28 +0530

    first

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('API Running');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function (req, res) {
+  console.log('Server started successfully');
+});
